cf05cbe61e257c6f7c9796c3a0e28b71bf189389
kavanap37 <kavanap12@gmail.com>
2025-02-23 17:02:41 +0530

    ValidateLogin

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 1. Create-react-app
 2. Tailwind setup- tailwindcss.com website=> documentation for create-react-app framework
+3. Router
+4. Sign in/ Sign Up : useState and toggle function
+5. useRef
+6. Form validation:
+   - regex
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Header from './Header'
+import { checkValidData } from "../utils/validateLogin";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const [errorMessage, setErrorMessage] = useState(null);
   const toggleSignInForm = () => {
       setIsSignInForm(!isSignInForm);
   };
+
+  const handleButtonClick = () => {
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+  }
+
   return (
     <div>
      <Header/>
      <div className="absolute">
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/0cf2c109-3af1-4a9d-87d7-aecfac5fe881/web/IN-en-20250217-TRIFECTA-perspective_c3376e06-9aff-4657-aafb-91256a597b7c_large.jpg" srcset="https://assets.nflxext.com/ffe/siteui/vlv3/0cf2c109-3af1-4a9d-87d7-aecfac5fe881/web/IN-en-20250217-TRIFECTA-perspective_c3376e06-9aff-4657-aafb-91256a597b7c_large.jpg 2000w, https://assets.nflxext.com/ffe/siteui/vlv3/0cf2c109-3af1-4a9d-87d7-aecfac5fe881/web/IN-en-20250217-TRIFECTA-perspective_c3376e06-9aff-4657-aafb-91256a597b7c_medium.jpg 1279w, https://assets.nflxext.com/ffe/siteui/vlv3/0cf2c109-3af1-4a9d-87d7-aecfac5fe881/web/IN-en-20250217-TRIFECTA-perspective_c3376e06-9aff-4657-aafb-91256a597b7c_small.jpg 959w" alt="logo" ></img>
     </div>
-    <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+    <form 
+     onSubmit={(e) => e.preventDefault()}
+    className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
         <h1 className="font-bold text-3xl py-4">
         {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
         {!isSignInForm && (
           <input
+            ref={name}
             type="text"
             placeholder="Full Name"
             className="p-4 my-4 w-full bg-gray-700"
@@ -25,15 +39,22 @@ const Login = () => {
         )}
         <input
           type="text"
+          ref={email}
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-700"
         />
         <input
           type="password"
+          ref={password}
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
         />
-         <button className="p-4 my-6 bg-red-700 w-full rounded-lg"> 
+        
+        <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+
+         <button className="p-4 my-6 bg-red-700 w-full rounded-lg"
+         onClick={handleButtonClick}
+         > 
          {isSignInForm ? "Sign In" : "Sign Up"}
          </button>
          <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
