c92caae729e7eef8e1a951ef18f8d4c9e5a8a4aa
kavanap37 <kavanap12@gmail.com>
2025-02-24 19:47:29 +0530

    TMDB API config and API call

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -6,3 +6,11 @@ export const USER_AVATAR =
   "https://occ-0-4994-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png";
 
 export const BG_URL="https://assets.nflxext.com/ffe/siteui/vlv3/0cf2c109-3af1-4a9d-87d7-aecfac5fe881/web/IN-en-20250217-TRIFECTA-perspective_c3376e06-9aff-4657-aafb-91256a597b7c_large.jpg"
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTdiOTRiOTEwOWQ0ZWUwZDQ0OTUzNDE3NzVlZjdiZCIsIm5iZiI6MTc0MDM5NzE5MS44NTIsInN1YiI6IjY3YmM1YTg3YTRiZjFjMTkyOGJmMDA2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vDeM5zJbWFINQk1Ok0wWbJYzvi4m28ZGCsz9c4MrWYU'
+  }
+};
