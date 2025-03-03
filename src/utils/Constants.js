0f858ad854ec2eede2d74d471c33dc38ffc8dd53
kavanap37 <kavanap12@gmail.com>
2025-03-03 21:00:15 +0530

    constant js recovery

---
+++ src/utils/Constants.js
@@ -0,0 +1,25 @@
+export const LOGO =
+"https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+
+export const USER_AVATAR =
+  "https://occ-0-4994-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png";
+
+export const BG_URL="https://assets.nflxext.com/ffe/siteui/vlv3/0cf2c109-3af1-4a9d-87d7-aecfac5fe881/web/IN-en-20250217-TRIFECTA-perspective_c3376e06-9aff-4657-aafb-91256a597b7c_large.jpg"
+
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTdiOTRiOTEwOWQ0ZWUwZDQ0OTUzNDE3NzVlZjdiZCIsIm5iZiI6MTc0MDM5NzE5MS44NTIsInN1YiI6IjY3YmM1YTg3YTRiZjFjMTkyOGJmMDA2OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vDeM5zJbWFINQk1Ok0wWbJYzvi4m28ZGCsz9c4MrWYU'
+  }
+};
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
+
+export const SUPPORTED_LANGUAGES = [
+  { identifier: "en", name: "English" },
+  { identifier: "hindi", name: "Hindi" },
+  { identifier: "spanish", name: "Spanish" },
+];
+
+export const OPENAI_KEY="API Key"
